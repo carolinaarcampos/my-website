637121321a51eb1575b3c431ef05df0445f50dda
caroliinaacampos <aguiarcamposcarolina@hotmail.com>
2022-11-15 23:15:18 +0000

    Organize path

--- CarolinaCampos/functions.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.slick-carousel').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1
-    });
-});
---
+++ CarolinaCampos/js/functions.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slick-carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1
+    });
+});
