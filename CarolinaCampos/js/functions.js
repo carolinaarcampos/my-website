b1ed08cb389bb0c3805ff3c13db37b4b9199f9e8
caroliinaacampos <aguiarcamposcarolina@hotmail.com>
2022-12-20 22:14:41 +0000

    Wip maintenance page and responsive

--- CarolinaCampos/js/functions.js
+++ CarolinaCampos/js/functions.js
@@ -6,46 +6,61 @@ $(document).ready(function(){
         slidesToShow: 1
     });
 
-    document.getElementById("first-content").addEventListener("click", function(){
-        const element = document.querySelector('.first-content');
+    if($('.vertical-buttons').length) {
+        document.getElementById("first-content").addEventListener("click", function(){
+            const element = document.querySelector('.first-content');
 
-        if(!element.classList.contains('active-content')) {
-            $('.first-content').addClass("active-content");
-            $('.second-content').removeClass("active-content");
-            $('.third-content').removeClass("active-content");
+            if(!element.classList.contains('active-content')) {
+                $('.first-content').addClass("active-content");
+                $('.second-content').removeClass("active-content");
+                $('.third-content').removeClass("active-content");
 
-            $('#first-content').addClass("button-active");
-            $('#second-content').removeClass("button-active");
-            $('#third-content').removeClass("button-active");
+                $('#first-content').addClass("button-active");
+                $('#second-content').removeClass("button-active");
+                $('#third-content').removeClass("button-active");
+            }
+        });
+
+        document.getElementById("second-content").addEventListener("click", function(){
+            const element = document.querySelector('.second-content');
+
+            if(!element.classList.contains('active-content')) {
+                $('.first-content').removeClass("active-content");
+                $('.second-content').addClass("active-content");
+                $('.third-content').removeClass("active-content");~
+
+                $('#first-content').removeClass("button-active");
+                $('#second-content').addClass("button-active");
+                $('#third-content').removeClass("button-active");
+            }
+        });
+
+        document.getElementById("third-content").addEventListener("click", function(){
+            const element = document.querySelector('.third-content');
+
+            if(!element.classList.contains('active-content')) {
+                $('.first-content').removeClass("active-content");
+                $('.second-content').removeClass("active-content");
+                $('.third-content').addClass("active-content");
+
+                $('#first-content').removeClass("button-active");
+                $('#second-content').removeClass("button-active");
+                $('#third-content').addClass("button-active");
+            }
+        });
+    }
+
+    document.getElementById("header-btn-mobile").addEventListener("click", function(){
+        const element = document.querySelector('.header-nav');
+
+        if(!element.classList.contains('-open')) {
+            $('.header-nav').addClass("-open");
+            $('.header-btn-mobile').addClass("-open");
+        } else {
+            $('.header-nav').removeClass("-open");
+            $('.header-btn-mobile').removeClass("-open");
         }
     });
 
-    document.getElementById("second-content").addEventListener("click", function(){
-        const element = document.querySelector('.second-content');
-
-        if(!element.classList.contains('active-content')) {
-            $('.first-content').removeClass("active-content");
-            $('.second-content').addClass("active-content");
-            $('.third-content').removeClass("active-content");~
-
-            $('#first-content').removeClass("button-active");
-            $('#second-content').addClass("button-active");
-            $('#third-content').removeClass("button-active");
-        }
-    });
-
-    document.getElementById("third-content").addEventListener("click", function(){
-        const element = document.querySelector('.third-content');
-
-        if(!element.classList.contains('active-content')) {
-            $('.first-content').removeClass("active-content");
-            $('.second-content').removeClass("active-content");
-            $('.third-content').addClass("active-content");
-
-            $('#first-content').removeClass("button-active");
-            $('#second-content').removeClass("button-active");
-            $('#third-content').addClass("button-active");
-        }
-    });
 });
 
